0f35f574c33f01d7a6a404772c30d7bf00ccd037
Sai Sudha <saisudha.0709@gmail.com>
2026-08-26 12:16:35 +0530

    Add user model

---
+++ src/User.java
@@ -0,0 +1,30 @@
+public class User {
+
+    private int userId;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(int userId, String name, String email, String password) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
