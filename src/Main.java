f2dbb1f80c33dda8c8fc4454332cd848cadff5d8
Sai Sudha <saisudha.0709@gmail.com>
2026-08-26 12:52:18 +0530

    Refactor banking services and update menu options
    Replaced BankingService with AccountService and updated menu options for user interactions.

--- src/Main.java
+++ src/Main.java
@@ -7,50 +7,44 @@ public class Main {
 
         UserService userService = new UserService();
         LoginService loginService = new LoginService();
-        BankingService bank = new BankingService();
-
-        double balance = 0;
+        AccountService accountService = new AccountService();
 
         System.out.println("===== SMART BANKING MANAGEMENT SYSTEM =====");
 
         System.out.println("1. Register");
         System.out.println("2. Login");
-        System.out.println("3. Deposit");
-        System.out.println("4. Withdraw");
-        System.out.println("5. Check Balance");
-        System.out.println("6. Exit");
+        System.out.println("3. Exit");
 
         System.out.print("Enter your choice: ");
         int choice = sc.nextInt();
         sc.nextLine();
 
         if (choice == 1) {
+
             userService.registerUser(sc);
 
         } else if (choice == 2) {
-            loginService.login(sc);
+
+            int userId = loginService.login(sc);
+
+            if (userId != -1) {
+                System.out.println("1. Create Bank Account");
+                System.out.println("2. Exit");
+
+                System.out.print("Enter your choice: ");
+                int accountChoice = sc.nextInt();
+
+                if (accountChoice == 1) {
+                    accountService.createAccount(sc, userId);
+                }
+            }
 
         } else if (choice == 3) {
-            System.out.print("Enter amount: ");
-            double amount = sc.nextDouble();
 
-            balance = bank.deposit(balance, amount);
-            System.out.println("Money deposited successfully");
-
-        } else if (choice == 4) {
-            System.out.print("Enter amount: ");
-            double amount = sc.nextDouble();
-
-            balance = bank.withdraw(balance, amount);
-            System.out.println("Withdrawal completed");
-
-        } else if (choice == 5) {
-            System.out.println("Balance: " + balance);
-
-        } else if (choice == 6) {
             System.out.println("Thank you for using Smart Banking System");
 
         } else {
+
             System.out.println("Invalid choice");
         }
 
